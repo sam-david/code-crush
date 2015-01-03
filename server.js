7a9c4f8f9e01b3443df728ad53eb56ed008857b0
Jarred <jarredhare@gmail.com>
2015-01-03 12:11:01 -0800

    Uncomment mongodb connect

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var session      = require('express-session');
 var configDB = require('./config/database.js');
 
 // configuration ===============================================================
-// mongoose.connect(configDB.url); // connect to our database
+mongoose.connect(configDB.url); // connect to our database
 
 require('./config/passport')(passport); // pass passport for configuration
 

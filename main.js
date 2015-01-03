c6f1a5a539d6d846c17b39cde02f926cf2cd753e
Apprentice <apprentice@DBC-infinity-too-3.local>
2015-01-03 12:46:48 -0800

    adds game score, multiplier, and city health

--- main.js
+++ main.js
@@ -20,12 +20,38 @@ var mainState = {
 		this.background = game.add.tileSprite(0, 0, this.game.width, this.game.height, 'space');
 
 		this.game.score = 0;
+		this.game.perfectCounter = 0;
+		this.game.multiplier = 1;
 		this.game.cityHealth = 5;
 
-		health = game.add.text(300,20, this.game.cityHealth);
+		codeText = game.add.text(250, 10, "var thing = 'thing'", { font: '34px Arial', fill: '#fff' });
+		cityHealthText = game.add.text(620,480, "Health: 5", {
+			font: "24px Arial",
+			fill: '#ff0044',
+			align: 'center'
+		});
+		gameScoreText = game.add.text(10,10, "Score: 0", {
+			font: "24px Arial",
+			fill: '#ff0044',
+			align: 'center'
+		});
+		multiplierText = game.add.text(620,10, "Multiplier: 1x", {
+			font: "24px Arial",
+			fill: '#ff0044',
+			align: 'center'
+		});
+
+		//Keyboard input for code...save for later
+		// this.game.input.keyboard.onDownCallback = function(e) {
+		// 	if (e.keyCode === 32) {
+		// 		// this.destroyComet;
+		// 	}
+		// }
+
+		this.laserKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		this.laserKey.onDown.add(this.destroyComet, this);
 
 
-		// gameTitle = game.add.text(300, 10, "Code City", { font: '34px Arial', fill: '#fff' });
 
 		// city = this.game.add.sprite(0,512, 'city');
 		this.city = this.game.add.sprite(0,512, 'city');
@@ -50,18 +76,30 @@ var mainState = {
 		if (this.game.cityHealth === 0) {
 			gameOver = game.add.text(300, 200, "Game Over", { font: '34px Arial', fill: '#fff' });
 		}
+		if (this.game.perfectCounter === 5) {
+			this.game.multiplier = 2;
+		} else if (this.game.perfectCounter === 10) {
+			this.game.multiplier = 3;
+		} else if (this.game.perfectCounter === 15) {
+			this.game.multiplier = 4;
+		} else if (this.game.perfectCounter === 20) {
+			this.game.multiplier = 5;
+		}
+		gameScoreText.setText("Score: " + this.game.score);
+		cityHealthText.setText("City Health: " + this.game.cityHealth);
+		multiplierText.setText("Multiplier: " + this.game.multiplier + "x");
 	},
 	dropComet: function() {
 		// makes an emitter for the comets
-		this.emitter = game.add.emitter(0, -50, 10);
-		this.emitter.makeParticles('fire1');
-		this.emitter.gravity = -100;
+		// this.emitter = game.add.emitter(0, -50, 10);
+		// this.emitter.makeParticles('fire1');
+		// this.emitter.gravity = -100;
 
 		//enable physics of comets
 		var comet;
 		this.comet = this.comets.create(this.game.world.randomX, 0, 'comet');
 		this.comet.enableBody = true;
-		this.comet.addChild(this.emitter);
+		// this.comet.addChild(this.emitter);
 		// this.game.physics.enable(this.comet, Phaser.Physics.ARCADE);
 
 		// game.physics.arcade.enable(this.comet);
@@ -75,11 +113,19 @@ var mainState = {
 		// comet.body.immovable = true;
 		this.comet.body.collideWorldBounds = true;
 
-		this.emitter.start(false, 100000000, 10, 30);
+		// this.emitter.start(false, 100000000, 10, 30);
+	},
+	destroyComet: function() {
+		console.log("destroy!");
+		this.comets.getAt(0).destroy();
+		this.game.score += (100 * this.game.multiplier);
+		this.game.perfectCounter += 1;
+		console.log(this.game.score);
 	},
 	hitCity: function() {
 		this.comets.getAt(0).destroy();
 		this.game.cityHealth -= 1;
+		this.game.perfectCounter = 0;
 		console.log(this.game.cityHealth);
 
 		// console.log(oldestComet);

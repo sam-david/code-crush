f91b2466fcabe1e1b17eb5d5d64f28842171c960
Jarred Hare <jarredhare@gmail.com>
2015-01-02 22:54:45 -0800

    Remove previous skeleton to start fresh with user authentication

--- README.md
+++
@@ -1,4 +0,0 @@
-code-crush
-==========
-
-It's like ping-pong, but for learning code on the internet.
--- models/Users.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var UserSchema = new mongoose.Schema({
-  email: String,
-  scores: [{type: mongoose.Schema.Types.ObjectId, ref: 'Score'}],
-  createdAt: {type: Date, default: Date.now}
-})
-
-mongoose.model('User', UserSchema);

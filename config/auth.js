e0b171f421c182029d1d4989bebd8baf5a9f3943
Jarred Hare <jarredhare@gmail.com>
2015-01-02 22:58:49 -0800

    New app skeleton with working auth, both local and facebook, no angular yet

---
+++ config/auth.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '915822205137081', // your App ID
+        'clientSecret'  : 'c9e9b22bb04799e48c48493244487854', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://admin:codecrush@ds029831.mongolab.com:29831/codecrush'
+
+};

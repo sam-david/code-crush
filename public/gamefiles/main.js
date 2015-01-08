add552666faa45813afd517aa3597bbfad384a85
Jarred <jarredhare@gmail.com>
2015-01-08 14:42:18 -0800

    adds loading bar

--- public/gamefiles/main.js
+++ public/gamefiles/main.js
@@ -3,8 +3,8 @@ var CodeFall = new Phaser.Game(960, 640, Phaser.AUTO, 'gameDiv');
 
 
 CodeFall.state.add('Boot', Boot);
+CodeFall.state.add('Preload', Preload);
 CodeFall.state.add('MainMenu', MainMenu);
 CodeFall.state.add('Game', Game);
-// CodeFall.state.add('Level1', Game);
 
 CodeFall.state.start('Boot');
